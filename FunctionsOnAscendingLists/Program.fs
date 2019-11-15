3a2d80c0250d1aabb421943af63b414c0ede261a
Carlos <carl7755@easv365.dk>
2019-11-15 11:22:52 +0100

    check if the list is weakly ascending method

--- FunctionsOnAscendingLists/Program.fs
+++ FunctionsOnAscendingLists/Program.fs
@@ -1,7 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+
+// Check if the list is weakly ascending:
+let checkWeakAscent weakList =
+    let rec checkWeakAscent' weakList current =
+        match weakList with
+        | [] -> true
+        | x :: [] -> current <= x
+        | x1 :: xs -> (current <= x1) && checkWeakAscent' xs x1
+    checkWeakAscent' weakList Int32.MinValue
