c1aa9090fcdaafbc68ddf6972ba5e302d0ed76e3
Carlos <carl7755@easv365.dk>
2019-11-15 11:10:39 +0100

    very initial commit

---
+++ FunctionsOnAscendingLists/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace FunctionsOnAscendingLists
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("FunctionsOnAscendingLists")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
